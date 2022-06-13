ef582e1f68e998d982137086ef5be9014af30c65
Retky <jose.arredondo.a@outlook.com>
2022-06-13 13:56:59 -0500

    Add 'getters & setters' for Person name, age, id

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
+  attr_reader :id # getter
+  attr_accessor :name, :age # getter & setter
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
