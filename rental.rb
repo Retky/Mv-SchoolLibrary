6ffee71c12f483f753d83611748595c54f92a20d
Retky <jose.arredondo.a@outlook.com>
2022-06-15 11:03:22 -0500

    Modify 'Rental' to belong Person & Book

--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    person.retnals << self
+    @book = book
+    book.rentals << self
   end
 end
