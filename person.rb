5fec9aa1d3bc489fe3f80c5069a14dae0a89c73a
Retky <jose.arredondo.a@outlook.com>
2022-06-15 11:02:49 -0500

    Add 'has many Rentals' to Book & Person

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './nameable'
 
 class Person < Nameable
-  attr_reader :id # getter
+  attr_reader :id, :rentals # getter
   attr_accessor :name, :age # getter & setter
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @rentals = []
   end
 
   def can_use_services?
