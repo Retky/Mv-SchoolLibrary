9badddeed804f7deb986d4d242f4f7236fac8acd
Retky <jose.arredondo.a@outlook.com>
2022-06-15 13:15:17 -0500

    Add 'add_rental' method to Book & Person

--- person.rb
+++ person.rb
@@ -1,4 +1,5 @@
 require_relative './nameable'
+require_relative './rental'
 
 class Person < Nameable
   attr_reader :id, :rentals # getter
@@ -19,6 +20,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    @rentals << Rental.new(date, self, book)
   end
 
   private
