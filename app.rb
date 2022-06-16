ce3dc98acd7d089541847e5a7491a2cfa4e11b8a
Retky <jose.arredondo.a@outlook.com>
2022-06-16 09:44:46 -0500

    Add 'menu' to App

--- app.rb
+++ app.rb
@@ -12,4 +12,15 @@ class App
     @rentals = []
   end
 
+  def menu
+    puts [
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
+    ]
+  end
 end
