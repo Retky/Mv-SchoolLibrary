6429d6541df67e2bbe5d6c8a49281696d98a08d6
Retky <jose.arredondo.a@outlook.com>
2022-06-16 09:55:54 -0500

    Add 'selection' to get user input

--- app.rb
+++ app.rb
@@ -12,6 +12,18 @@ class App
     @rentals = []
   end
 
+  def selection(input)
+    case input
+    when 1 then list_books
+    when 2 then list_persons
+    when 3 then create_person
+    when 4 then create_book
+    when 5 then create_rental
+    when 6 then list_rentals
+    when 7 then exit
+    end
+  end
+
   def menu
     puts [
       '1 - List all books',
@@ -22,5 +34,6 @@ class App
       '6 - List all rentals for a given person id',
       '7 - Exit'
     ]
+    puts selection(gets.chomp)
   end
 end
